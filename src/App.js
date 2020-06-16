4ae900d34b67eddc59abc24b2dcf66a5892e7f1a
Luciano dii Souza <lucianodiisouza@hotmail.com>
2020-06-15 23:26:07 -0300

    start: setting eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>RocketShoes</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>RocketShoes</h1>
+        </div>
+    );
 }
 
 export default App;
