e84211baba0e8e33a996e2bac11a7615e6edc024
Luciano dii Souza <lucianodiisouza@hotmail.com>
2020-06-16 02:21:32 -0300

    dev: Adding ADD_TO_CART action to cart reducer, adding product count based on product.length in this.state

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
     constructor(props) {
         super(props);
 
@@ -24,6 +25,15 @@ export default class Home extends Component {
         this.setState({ products: data });
     }
 
+    handleAddProduct = (product) => {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'ADD_TO_CART',
+            product,
+        });
+    };
+
     render() {
         const { products } = this.state;
 
@@ -35,7 +45,10 @@ export default class Home extends Component {
                         <strong>{product.title}</strong>
                         <span>{product.priceFormatted}</span>
 
-                        <button type="button">
+                        <button
+                            type="button"
+                            onClick={() => this.handleAddProduct(product)}
+                        >
                             <div>
                                 <MdAddShoppingCart size={16} color="#fff" /> 3
                             </div>
@@ -48,3 +61,5 @@ export default class Home extends Component {
         );
     }
 }
+
+export default connect()(Home);
