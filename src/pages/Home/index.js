9659c013be86eefbb397c8c03630fac9e3542d8d
Luciano dii Souza <lucianodiisouza@hotmail.com>
2020-06-16 00:46:36 -0300

    dev: styling ProductList and adding polished library

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <div />;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-mizuno-mirai-3-masculino/20/D16-5542-120/D16-5542-120_detalhe1.jpg?ts=1580228536?ims=280x280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-mizuno-mirai-3-masculino/20/D16-5542-120/D16-5542-120_detalhe1.jpg?ts=1580228536?ims=280x280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-mizuno-mirai-3-masculino/20/D16-5542-120/D16-5542-120_detalhe1.jpg?ts=1580228536?ims=280x280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-mizuno-mirai-3-masculino/20/D16-5542-120/D16-5542-120_detalhe1.jpg?ts=1580228536?ims=280x280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-mizuno-mirai-3-masculino/20/D16-5542-120/D16-5542-120_detalhe1.jpg?ts=1580228536?ims=280x280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-mizuno-mirai-3-masculino/20/D16-5542-120/D16-5542-120_detalhe1.jpg?ts=1580228536?ims=280x280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
