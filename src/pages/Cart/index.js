86d2b06d1401ca29cb08434883c9d85f46afcf5f
Luciano dii Souza <lucianodiisouza@hotmail.com>
2020-06-16 23:12:07 -0300

    dev: List cart info

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
     MdRemoveCircleOutline,
     MdAddCircleOutline,
@@ -6,7 +7,7 @@ import {
 } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart }) {
     return (
         <Container>
             <ProductTable>
@@ -20,43 +21,42 @@ export default function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <img
-                                src="https://static.netshoes.com.br/produtos/tenis-mizuno-mirai-3-masculino/20/D16-5542-120/D16-5542-120_detalhe1.jpg?ts=1580228536?ims=280x280"
-                                alt="Tênis"
-                            />
-                        </td>
-                        <td>
-                            <strong>Tenis muito massa</strong>
-                            <span>R$ 129,90</span>
-                        </td>
-                        <td>
-                            <div>
+                    {cart.map((product) => (
+                        <tr>
+                            <td>
+                                <img src={product.image} alt={product.title} />
+                            </td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormatted}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="button">
+                                        <MdRemoveCircleOutline
+                                            size={20}
+                                            color="#069"
+                                        />
+                                    </button>
+                                    <input type="number" value={product.amount} />
+                                    <button type="button">
+                                        <MdAddCircleOutline
+                                            size={20}
+                                            color="#069"
+                                        />
+                                    </button>
+                                </div>
+                            </td>
+                            <td>
+                                <strong>R$ 259,80</strong>
+                            </td>
+                            <td>
                                 <button type="button">
-                                    <MdRemoveCircleOutline
-                                        size={20}
-                                        color="#069"
-                                    />
+                                    <MdDelete size={20} color="#069" />
                                 </button>
-                                <input type="number" value={1} />
-                                <button type="button">
-                                    <MdAddCircleOutline
-                                        size={20}
-                                        color="#069"
-                                    />
-                                </button>
-                            </div>
-                        </td>
-                        <td>
-                            <strong>R$ 259,80</strong>
-                        </td>
-                        <td>
-                            <button type="button">
-                                <MdDelete size={20} color="#069" />
-                            </button>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </ProductTable>
 
@@ -71,3 +71,8 @@ export default function Cart() {
         </Container>
     );
 }
+
+const mapStateToProps = (state) => ({
+    cart: state.cart,
+});
+export default connect(mapStateToProps)(Cart);
